0d170c3e3f050c919c9fafbaac58fea70c0760cf
Eric Meyer <eemeyer@gmail.com>
2015-02-24 17:48:08 -0500

    Adds QueryStringUtils - extracted from homeland.

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = {
   HttpHelper: require('./lib/HttpHelper'),
   DOMUtils: require('./lib/DOMUtils'),
   ScheduledFunction: require('./lib/ScheduledFunction'),
+  QueryStringUtils: require('./lib/QueryStringUtils')
 };
